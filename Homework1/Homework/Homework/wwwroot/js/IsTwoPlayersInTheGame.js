7eabdc27c171f5627f93438980c8a7d9fa21a5f8
DmitriySurnov <79646813832@yandex.ru>
2023-07-09 20:28:57 +0300

    Некоторый рефакторинг

---
+++ Homework1/Homework/Homework/wwwroot/js/IsTwoPlayersInTheGame.js
@@ -0,0 +1,15 @@
+﻿function AjaxRecheck() {
+    console.log("IsTwoPlayersInTheGame");
+    $.ajax({
+        type: 'POST',
+        url: '.. /../../Api/IsTwoPlayersInTheGame',
+        success: function (result) {
+            console.log("Data receiver");
+            console.log(result);
+            if (!result) {
+                $("#WaitingPlayers").trigger("click")
+            }
+        }
+    })
+}
+var t = setInterval(AjaxRecheck, 1000);
